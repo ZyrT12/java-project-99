65b9fb66743d6aa988f0f220c9f72a0a04f95c93
ZyrT12 <Zyryanova554@gmail.com>
2025-08-14 21:38:40 +0500

    fix tests

--- app/src/main/java/hexlet/code/dto/tasks/TaskResponseDto.java
+++ app/src/main/java/hexlet/code/dto/tasks/TaskResponseDto.java
@@ -1,17 +1,11 @@
 package hexlet.code.dto.tasks;
 
 import java.time.LocalDate;
-import java.util.List;
 
-public class TaskResponseDto {
+public class TaskResponseDto extends TaskBaseDto {
     private Long id;
     private Integer index;
     private LocalDate createdAt;
-    private String title;
-    private String description;
-    private Long executorId;
-    private Long taskStatusId;
-    private List<Long> labelIds;
 
     public Long getId() {
         return id;
@@ -25,26 +19,6 @@ public class TaskResponseDto {
         return createdAt;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public Long getExecutorId() {
-        return executorId;
-    }
-
-    public Long getTaskStatusId() {
-        return taskStatusId;
-    }
-
-    public List<Long> getLabelIds() {
-        return labelIds;
-    }
-
     public void setId(Long id) {
         this.id = id;
     }
@@ -55,25 +29,5 @@ public class TaskResponseDto {
 
     public void setCreatedAt(LocalDate createdAt) {
         this.createdAt = createdAt;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setExecutorId(Long executorId) {
-        this.executorId = executorId;
-    }
-
-    public void setTaskStatusId(Long taskStatusId) {
-        this.taskStatusId = taskStatusId;
-    }
-
-    public void setLabelIds(List<Long> labelIds) {
-        this.labelIds = labelIds;
     }
 }
--- app/src/main/java/hexlet/code/dto/tasks/TaskUpsertDto.java
+++ app/src/main/java/hexlet/code/dto/tasks/TaskUpsertDto.java
@@ -2,54 +2,12 @@ package hexlet.code.dto.tasks;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import java.util.List;
 
-public class TaskUpsertDto {
+public class TaskUpsertDto extends TaskBaseDto {
+    @Override
     @NotBlank(groups = OnCreate.class)
     @Size(min = 1, max = 100, groups = OnCreate.class)
-    private String title;
-    private String description;
-    private Long executorId;
-    private Long taskStatusId;
-    private List<Long> labelIds;
-
     public String getTitle() {
-        return title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public Long getExecutorId() {
-        return executorId;
-    }
-
-    public Long getTaskStatusId() {
-        return taskStatusId;
-    }
-
-    public List<Long> getLabelIds() {
-        return labelIds;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setExecutorId(Long executorId) {
-        this.executorId = executorId;
-    }
-
-    public void setTaskStatusId(Long taskStatusId) {
-        this.taskStatusId = taskStatusId;
-    }
-
-    public void setLabelIds(List<Long> labelIds) {
-        this.labelIds = labelIds;
+        return super.getTitle();
     }
 }
