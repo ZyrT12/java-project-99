73627ae8e3bac74b5bd67a4f3efbc7a4f7272cc0
ZyrT12 <Zyryanova554@gmail.com>
2025-08-21 13:04:42 +0500

    fix deploy

--- src/main/resources/db/migration/V11__fix_labels_created_at_type.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE public.labels
-ALTER COLUMN created_at TYPE timestamptz
-    USING (created_at::timestamp AT TIME ZONE 'UTC');
-
-ALTER TABLE public.labels
-    ALTER COLUMN created_at SET DEFAULT now();
-
-ALTER TABLE public.labels
-    ALTER COLUMN created_at SET NOT NULL;
