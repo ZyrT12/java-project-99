a85e45d6fad09223329c9068df6a14e448e23344
ZyrT12 <Zyryanova554@gmail.com>
2025-08-22 14:00:22 +0500

    FIX token

--- src/main/java/hexlet/code/dto/labels/LabelResponseDto.java
+++ src/main/java/hexlet/code/dto/labels/LabelResponseDto.java
@@ -2,41 +2,11 @@ package hexlet.code.dto.labels;
 
 import java.time.Instant;
 
-public class LabelResponseDto {
-    private Long id;
-    private String name;
-    private String slug;
-    private Instant createdAt;
+public record LabelResponseDto(
+        Long id,
+        String name,
+        String slug,
+        Instant createdAt
+) {
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getSlug() {
-        return slug;
-    }
-
-    public Instant getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setSlug(String slug) {
-        this.slug = slug;
-    }
-
-    public void setCreatedAt(Instant createdAt) {
-        this.createdAt = createdAt;
-    }
 }
--- src/test/java/hexlet/code/dto/labels/LabelResponseDtoTest.java
+++ src/test/java/hexlet/code/dto/labels/LabelResponseDtoTest.java
@@ -2,22 +2,19 @@ package hexlet.code.dto.labels;
 
 import java.time.Instant;
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LabelResponseDtoTest {
 
     @Test
-    void gettersAndSettersWork() {
-        LabelResponseDto dto = new LabelResponseDto();
-        dto.setId(1L);
-        dto.setName("Bug");
-        dto.setSlug("bug");
+    void ctorAndAccessorsWork() {
         Instant now = Instant.now();
-        dto.setCreatedAt(now);
+        LabelResponseDto dto = new LabelResponseDto(1L, "Bug", "bug", now);
 
-        assertThat(dto.getId()).isEqualTo(1L);
-        assertThat(dto.getName()).isEqualTo("Bug");
-        assertThat(dto.getSlug()).isEqualTo("bug");
-        assertThat(dto.getCreatedAt()).isEqualTo(now);
+        assertThat(dto.id()).isEqualTo(1L);
+        assertThat(dto.name()).isEqualTo("Bug");
+        assertThat(dto.slug()).isEqualTo("bug");
+        assertThat(dto.createdAt()).isEqualTo(now);
     }
 }
