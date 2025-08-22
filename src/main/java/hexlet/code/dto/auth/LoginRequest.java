3fa33c78bf371478f830929a2ba0b8752a752b04
ZyrT12 <Zyryanova554@gmail.com>
2025-08-22 13:33:17 +0500

    FIX token

--- src/main/java/hexlet/code/dto/auth/LoginRequest.java
+++ src/main/java/hexlet/code/dto/auth/LoginRequest.java
@@ -28,12 +28,10 @@ public class LoginRequest {
     }
 
     public String getPassword() {
-
         return password;
     }
 
     public void setPassword(String password) {
-
         this.password = password;
     }
 }
